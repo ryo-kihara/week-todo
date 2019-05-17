edde52a14746990c1fba37867ea88bb646353462
ryo-kihara <48352529+ryo-kihara@users.noreply.github.com>
2019-05-17 18:45:29 +0900

    Update main.js

--- main.js
+++ main.js
@@ -6,18 +6,22 @@ $(function() {
   var todo = '<div class="todo"></div>';
   var day = $('.day');
   var selectDay = $('.selectday');
-  var week = $('.day-of-the-week');
+  var deletion = $('.deletion');
+  var achive = $('.achive');
+  var deleteAll = $('.deleteAll');
+  var inputbtn = $('.inputbtn');
+  var activeDay;
+  var achiveCount = 0;
   // var active = $('.active'); ミス
-  var already = 0;
 
   // 今日の曜日
-  var today = new Date().getDay();
-  if (today === 0) {
-    today = 7;
+  var todayNumber = new Date().getDay();
+  if (todayNumber === 0) {
+    todayNumber = 7;
   }
-  $('.day-of-the-week li:nth-child('+ today +')').addClass('selectday');
-  $('main div:nth-child('+ today +')').addClass('active');
-
+  $('.day-of-the-week li:nth-child('+ todayNumber +')').addClass('selectday');
+  $('main div:nth-child('+ todayNumber +')').addClass('active');
+  activeDay = $('#' + $('main div:nth-child('+ todayNumber +')').attr('id'));
 
   // todo追加
   btn.click(function() {
@@ -36,12 +40,13 @@ $(function() {
 
   // 曜日選択
   day.click(function() {
+    activeDay = $('#' + $(this).data('id'));
+
     for (var i = 0; i < day.length; i++) {
       if ($(day[i]).hasClass('day selectday')) {
         $(day[i]).removeClass('selectday');
       }
     }
-
     $(this).addClass('selectday');
 
     for (var i = 0; i < content.length; i++) {
@@ -49,8 +54,43 @@ $(function() {
         $(content[i]).removeClass('active');
       }
     }
+    activeDay.attr('class', 'content active');
+  });
 
-    $('#' + $(this).data('id')).attr('class', 'content active');
+  // 削除
+  deletion.click(function() {
+    activeDay.children('.todo.already').remove();
+  });
+
+  // 達成度計算
+  achive.click(function() {
+    achiveCount++;
+    console.log(achiveCount);
+    var percent = '<p class="percent"></p>';
+
+    if (achiveCount % 2 === 0) {
+      $(this).children('.percent').remove();
+      $(this).append('<i class="fas fa-percent"></i>');
+    }
+    else if (achiveCount % 2 === 1) {
+      var score = (activeDay.children('.todo.already').length / activeDay.children('.todo').length) * 100;
+      if (isNaN(score)) {
+        return false;
+      }
+      $(this).children('i').remove();
+      $(percent).text(Math.ceil(score)).appendTo($(this)).hide().fadeIn(500);
+    }
+  });
+
+  // 全削除
+  deleteAll.click(function() {
+    activeDay.children('.todo').remove();
+  });
+
+  // 入力
+  inputbtn.click(function() {
+    input.toggle();
+    btn.toggle();
   });
 
 });
